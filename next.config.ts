a5fd9ca17fd6a5c7eba50e63e710fb32dda26de7
Dustin McCaffree <dustinnicholasmccaffree@gmail.com>
2026-01-20 13:59:00 -0600

    Fix config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	images: {
-		domains: ["file.swell.so"],
+		remotePatterns: [{ hostname: "file.swell.so", protocol: "https" }],
 	},
 };
 
