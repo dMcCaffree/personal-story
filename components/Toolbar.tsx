910e9c7abcc1a68df5517e99ebae800c3e04f7ac
Dustin McCaffree <dustinnicholasmccaffree@gmail.com>
2026-01-21 13:57:06 -0600

    Add real resume

--- components/Toolbar.tsx
+++ components/Toolbar.tsx
@@ -13,7 +13,8 @@ import { Tooltip } from "@/components/Tooltip";
 import Image from "next/image";
 
 const LINKEDIN_URL = "https://linkedin.com/in/dMcCaffree";
-const RESUME_URL = "#"; // Placeholder for now
+const RESUME_URL =
+	"https://file.swell.so/story/documents/Dustin_McCaffree_Resume_2026.pdf";
 const EMAIL = "mailto:dustin@terrible.app";
 const MAX_TITLE_LENGTH_BEFORE_ANIMATING = 50;
 
@@ -426,7 +427,7 @@ export function Toolbar() {
 						strokeLinecap="round"
 						strokeLinejoin="round"
 						strokeWidth={2}
-						d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
+						d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"
 					/>
 				</svg>
 			),
